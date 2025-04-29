869783110665b29e3b27917b02958700306f07ca
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-04-29 22:57:29 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,12 @@
+// DBConnection.java
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
