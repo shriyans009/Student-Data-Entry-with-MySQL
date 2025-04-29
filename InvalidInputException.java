03fcf27270e1f4ff5e2b218441bfcdcf8919b48f
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-04-29 22:59:12 +0530

    Create InvalidInputException.java

---
+++ InvalidInputException.java
@@ -0,0 +1,8 @@
+// InvalidInputException.java
+
+// Custom Exception for invalid input
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
