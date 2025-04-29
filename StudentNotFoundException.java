97796040f03d1d949cfe8f620d9b84166d6f7915
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-04-29 23:04:32 +0530

    Create StudentNotFoundException.java

---
+++ StudentNotFoundException.java
@@ -0,0 +1,8 @@
+// StudentNotFoundException.java
+
+// Custom Exception for student not found scenario
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
